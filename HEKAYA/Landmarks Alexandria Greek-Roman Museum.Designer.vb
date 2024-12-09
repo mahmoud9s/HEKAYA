80fea9ffab81783c1f96dad9766063e17f25ba64
malak-0730 <malakalaa07@yahoo.com>
2024-12-09 21:00:53 +0200

    Alexandria Landmarks: Bibliotheca Alexandrina,Greek-Roman Museum,Qaitbay Citadel,Stanley Bridge

---
+++ HEKAYA/Landmarks Alexandria Greek-Roman Museum.Designer.vb
@@ -0,0 +1,419 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Landmarks_Alexandria_Greek_Roman_Museum
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Landmarks_Alexandria_Greek_Roman_Museum))
+        Label13 = New Label()
+        PictureBox4 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        Label16 = New Label()
+        Label15 = New Label()
+        Label14 = New Label()
+        Label10 = New Label()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label6 = New Label()
+        LinkLabel1 = New LinkLabel()
+        PictureBox6 = New PictureBox()
+        Label5 = New Label()
+        PictureBox5 = New PictureBox()
+        Label2 = New Label()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        PictureBox8 = New PictureBox()
+        PictureBox7 = New PictureBox()
+        PictureBox9 = New PictureBox()
+        PictureBox10 = New PictureBox()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label13
+        ' 
+        Label13.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Label13.AutoSize = True
+        Label13.BackColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label13.Cursor = Cursors.Hand
+        Label13.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold)
+        Label13.ForeColor = Color.White
+        Label13.Location = New Point(1773, 164)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(106, 25)
+        Label13.TabIndex = 248
+        Label13.Text = "Book now"
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox4.BackgroundImage = CType(resources.GetObject("PictureBox4.BackgroundImage"), Image)
+        PictureBox4.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox4.Cursor = Cursors.Hand
+        PictureBox4.Location = New Point(1750, 147)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(142, 65)
+        PictureBox4.TabIndex = 247
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), Image)
+        PictureBox2.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox2.Cursor = Cursors.Hand
+        PictureBox2.Location = New Point(1609, 157)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(52, 42)
+        PictureBox2.TabIndex = 246
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), Image)
+        PictureBox3.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox3.Cursor = Cursors.Hand
+        PictureBox3.Location = New Point(1680, 154)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(51, 50)
+        PictureBox3.TabIndex = 245
+        PictureBox3.TabStop = False
+        ' 
+        ' Label16
+        ' 
+        Label16.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label16.Font = New Font("Microsoft Sans Serif", 20F)
+        Label16.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label16.Location = New Point(33, 851)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(658, 43)
+        Label16.TabIndex = 242
+        Label16.Text = "Typically open from 9:00 AM to 5:00 PM."
+        ' 
+        ' Label15
+        ' 
+        Label15.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label15.AutoSize = True
+        Label15.Font = New Font("Microsoft Sans Serif", 24F, FontStyle.Bold)
+        Label15.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label15.Location = New Point(15, 784)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(351, 46)
+        Label15.TabIndex = 241
+        Label15.Text = "Operating Hours :"
+        ' 
+        ' Label14
+        ' 
+        Label14.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label14.AutoSize = True
+        Label14.Font = New Font("Microsoft Sans Serif", 20F, FontStyle.Bold)
+        Label14.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label14.Location = New Point(47, 894)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(0, 39)
+        Label14.TabIndex = 240
+        ' 
+        ' Label10
+        ' 
+        Label10.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label10.AutoSize = True
+        Label10.Font = New Font("Microsoft Sans Serif", 24F, FontStyle.Bold)
+        Label10.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label10.Location = New Point(543, 829)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(0, 46)
+        Label10.TabIndex = 238
+        ' 
+        ' Label8
+        ' 
+        Label8.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label8.Font = New Font("Microsoft Sans Serif", 20F)
+        Label8.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label8.Location = New Point(33, 676)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(987, 88)
+        Label8.TabIndex = 237
+        Label8.Text = "Standard ticket prices vary for local and international visitors. Reduced prices are available for students."
+        ' 
+        ' Label7
+        ' 
+        Label7.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label7.Font = New Font("Microsoft Sans Serif", 24F, FontStyle.Bold)
+        Label7.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label7.Location = New Point(15, 621)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(288, 55)
+        Label7.TabIndex = 236
+        Label7.Text = "Entrance Fee :"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Cursor = Cursors.Hand
+        Label4.Font = New Font("Microsoft Sans Serif", 12F)
+        Label4.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label4.Location = New Point(136, 64)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(131, 25)
+        Label4.TabIndex = 235
+        Label4.Text = "Landmarks  >"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Cursor = Cursors.Hand
+        Label3.Font = New Font("Microsoft Sans Serif", 12F)
+        Label3.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label3.Location = New Point(263, 64)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(215, 25)
+        Label3.TabIndex = 234
+        Label3.Text = "Greek-Roman Museum"
+        ' 
+        ' Label6
+        ' 
+        Label6.Font = New Font("Lucida Sans Unicode", 22.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label6.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label6.Location = New Point(10, 232)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(1198, 389)
+        Label6.TabIndex = 233
+        Label6.Text = resources.GetString("Label6.Text")
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Font = New Font("Segoe UI", 20F)
+        LinkLabel1.LinkColor = SystemColors.HotTrack
+        LinkLabel1.Location = New Point(71, 183)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(365, 46)
+        LinkLabel1.TabIndex = 232
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Greek-Roman Museum"
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.BackColor = Color.Transparent
+        PictureBox6.BackgroundImage = CType(resources.GetObject("PictureBox6.BackgroundImage"), Image)
+        PictureBox6.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox6.Cursor = Cursors.Hand
+        PictureBox6.Location = New Point(15, 181)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(50, 48)
+        PictureBox6.TabIndex = 231
+        PictureBox6.TabStop = False
+        ' 
+        ' Label5
+        ' 
+        Label5.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left
+        Label5.AutoSize = True
+        Label5.Font = New Font("Mongolian Baiti", 46F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label5.Location = New Point(10, 96)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(770, 82)
+        Label5.TabIndex = 230
+        Label5.Text = "Greek-Roman Museum"
+        ' 
+        ' PictureBox5
+        ' 
+        PictureBox5.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), Image)
+        PictureBox5.Location = New Point(1214, 221)
+        PictureBox5.Name = "PictureBox5"
+        PictureBox5.Size = New Size(689, 812)
+        PictureBox5.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox5.TabIndex = 229
+        PictureBox5.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Label2.AutoSize = True
+        Label2.BackColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label2.Cursor = Cursors.Hand
+        Label2.Font = New Font("Microsoft Sans Serif", 12F, FontStyle.Bold)
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(1767, 96)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(0, 25)
+        Label2.TabIndex = 228
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), Image)
+        PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox1.Cursor = Cursors.Hand
+        PictureBox1.Location = New Point(1214, 164)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(179, 51)
+        PictureBox1.TabIndex = 227
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Cursor = Cursors.Hand
+        Label1.Font = New Font("Microsoft Sans Serif", 12F)
+        Label1.ForeColor = Color.FromArgb(CByte(98), CByte(62), CByte(42))
+        Label1.Location = New Point(6, 64)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(138, 25)
+        Label1.TabIndex = 226
+        Label1.Text = "Alexandrina > "
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.BackColor = Color.Transparent
+        PictureBox8.BackgroundImage = My.Resources.Resources.Picture7
+        PictureBox8.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox8.Cursor = Cursors.Hand
+        PictureBox8.Location = New Point(-1, -1)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(80, 51)
+        PictureBox8.TabIndex = 225
+        PictureBox8.TabStop = False
+        ' 
+        ' PictureBox7
+        ' 
+        PictureBox7.BackColor = Color.Transparent
+        PictureBox7.BackgroundImage = My.Resources.Resources.Picture6
+        PictureBox7.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox7.Cursor = Cursors.Hand
+        PictureBox7.Location = New Point(98, -1)
+        PictureBox7.Name = "PictureBox7"
+        PictureBox7.Size = New Size(63, 51)
+        PictureBox7.TabIndex = 224
+        PictureBox7.TabStop = False
+        ' 
+        ' PictureBox9
+        ' 
+        PictureBox9.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox9.BackColor = Color.Transparent
+        PictureBox9.BackgroundImage = My.Resources.Resources.love
+        PictureBox9.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox9.Cursor = Cursors.Hand
+        PictureBox9.Location = New Point(1730, -1)
+        PictureBox9.Name = "PictureBox9"
+        PictureBox9.Size = New Size(69, 51)
+        PictureBox9.TabIndex = 223
+        PictureBox9.TabStop = False
+        ' 
+        ' PictureBox10
+        ' 
+        PictureBox10.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox10.BackColor = Color.Transparent
+        PictureBox10.BackgroundImage = My.Resources.Resources.Picture9
+        PictureBox10.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox10.Cursor = Cursors.Hand
+        PictureBox10.Location = New Point(1816, -1)
+        PictureBox10.Name = "PictureBox10"
+        PictureBox10.Size = New Size(63, 51)
+        PictureBox10.TabIndex = 222
+        PictureBox10.TabStop = False
+        ' 
+        ' Landmarks_Alexandria_Greek_Roman_Museum
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1902, 1033)
+        Controls.Add(Label13)
+        Controls.Add(PictureBox4)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox3)
+        Controls.Add(Label16)
+        Controls.Add(Label15)
+        Controls.Add(Label14)
+        Controls.Add(Label10)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label6)
+        Controls.Add(LinkLabel1)
+        Controls.Add(PictureBox6)
+        Controls.Add(Label5)
+        Controls.Add(PictureBox5)
+        Controls.Add(Label2)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox8)
+        Controls.Add(PictureBox7)
+        Controls.Add(PictureBox9)
+        Controls.Add(PictureBox10)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Landmarks_Alexandria_Greek_Roman_Museum"
+        Text = "Landmarks_Alexandria_Greek_Roman_Museum"
+        WindowState = FormWindowState.Maximized
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label13 As Label
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+End Class
